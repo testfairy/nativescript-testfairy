cd20874da69f774459f12ac2cfe0fad280f78e9b
Vijay Sharma <vijay.sharm@gmail.com>
2019-04-15 09:54:38 -0400

    Update nativescript

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,8 +1,14 @@
-﻿import * as application from 'tns-core-modules/application';
-import { TestFairySDK } from 'nativescript-testfairy';
+/*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
 
-application.on(application.launchEvent, (args) => {
-    TestFairySDK.begin("5b3af35e59a1e074e2d50675b1b629306cf0cfbd");
-});
+import * as app from "tns-core-modules/application";
 
-application.start({ moduleName: "main-page" });
+app.run({ moduleName: "app-root" });
+
+/*
+Do not place any code after the application has been started as it will not
+be executed on iOS.
+*/
