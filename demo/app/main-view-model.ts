fb7271677b6e4af974afdc5e5901aa82eb6ad2fe
Vijay Sharma <vijay.sharm@gmail.com>
2020-11-26 22:08:18 -0500

    Restore original project

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,39 +1,50 @@
 import { Observable } from '@nativescript/core';
+import { TestFairySDK } from 'nativescript-testfairy';
+
+class DataItem {
+    constructor(public name: string) { }
+}
 
 export class HelloWorldModel extends Observable {
-    private _counter: number;
-    private _message: string;
+  public myItems: Array<DataItem>;
 
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.myItems = [
+    	new DataItem("begin"),
+    	new DataItem("log"),
+    	new DataItem("setAttribute"),
+    	new DataItem("setUserId"),
+    	new DataItem("pause"),
+    	new DataItem("resume"),
+    	new DataItem("stop"),
+    	new DataItem("hideView"),
+    ];
+  }
 
-        // Initialize default values.
-        this._counter = 42;
-        this.updateMessage();
-    }
+  public onTap(args) {
+	var itemIndex = args.index;
+	console.log("index " + itemIndex);
 
-    get message(): string {
-        return this._message;
-    }
-
-    set message(value: string) {
-        if (this._message !== value) {
-            this._message = value;
-            this.notifyPropertyChange('message', value);
-        }
-    }
-
-    onTap() {
-        this._counter--;
-        this.updateMessage();
-    }
-
-    private updateMessage() {
-        if (this._counter <= 0) {
-            this.message =
-                'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-        } else {
-            this.message = `${this._counter} taps left`;
-        }
-    }
+	if (itemIndex == 0) {
+		// TestFairySDK.begin("5b3af35e59a1e074e2d50675b1b629306cf0cfbd");
+	} else if (itemIndex == 1) {
+		TestFairySDK.log("new message");
+	} else if (itemIndex == 2) {
+		TestFairySDK.setAttribute("attribute", "new attribute");
+	} else if (itemIndex == 3) {
+		TestFairySDK.setUserId("support@testfairy.com");
+	} else if (itemIndex == 4) {
+		TestFairySDK.pause();
+	} else if (itemIndex == 5) {
+		TestFairySDK.resume();
+	} else if (itemIndex == 6) {
+		TestFairySDK.stop();
+	} else if (itemIndex == 7) {
+		var page = args.object;
+  		TestFairySDK.hideView(page);
+	} else {
+		console.log("unsupported index " + itemIndex);
+	}
+  }
 }
